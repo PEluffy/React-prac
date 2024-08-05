e55085d586421d6d6158e663300160a1d4902514
Prashannakafle4444 <kafleprakrit@gmail.com>
2024-08-05 12:01:18 +0545

    state learning

--- steps/src/App.js
+++ steps/src/App.js
@@ -8,46 +8,54 @@ const messages = [
 
 function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={`number one ${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`number two ${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`number one ${step >= 3 ? "active" : ""}`}>3</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          className="btn"
-          onClick={() => {
-            setStep((pre) => {
-              if (pre === 1) {
-                return pre;
-              } else {
-                return pre - 1;
-              }
-            });
-          }}
-        >
-          Previous
-        </button>
-        <button
-          className="btn"
-          onClick={() => {
-            setStep((pre) => {
-              if (pre === 3) {
-                return pre;
-              } else {
-                return pre + 1;
-              }
-            });
-          }}
-        >
-          Next
-        </button>
-      </div>
+    <div className="container">
+      <button className="close " onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`number one ${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`number two ${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`number one ${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              className="btn"
+              onClick={() => {
+                setStep((pre) => {
+                  if (pre === 1) {
+                    return pre;
+                  } else {
+                    return pre - 1;
+                  }
+                });
+              }}
+            >
+              Previous
+            </button>
+            <button
+              className="btn"
+              onClick={() => {
+                setStep((pre) => {
+                  if (pre === 3) {
+                    return pre;
+                  } else {
+                    return pre + 1;
+                  }
+                });
+              }}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
