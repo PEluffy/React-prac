09416c91a2fcd6afe904f1963a18e2539aa66c8b
Prashannakafle4444 <kafleprakrit@gmail.com>
2024-08-06 17:53:40 +0545

    some changes

--- outing-list/src/components/Form.js
+++ outing-list/src/components/Form.js
@@ -2,15 +2,27 @@ import { useState } from "react";
 
 function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
   function handlsubmit(e) {
     e.preventDefault();
+    if (!description) return;
+    console.log("submitted");
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
   }
 
   return (
     <form className="add-form" onSubmit={handlsubmit}>
       <h3>What do you need for your😍trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onInput={(e) => {
+          setQuantity(Number(e.target.value));
+        }}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
