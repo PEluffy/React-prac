9fa7eb7d5feee74396ff2abff7b0d4d9d4a1560d
Prashannakafle4444 <kafleprakrit@gmail.com>
2025-02-15 11:19:50 +0545

    Cities and Countries list

--- worldwise/src/components/CityList.jsx
+++ worldwise/src/components/CityList.jsx
@@ -1,15 +1,19 @@
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
 import CityItem from "./CityItem";
+import Message from "./Message";
 
 function CityList({ cities, isLoading }) {
   if (isLoading) {
     return <Spinner />;
   }
+  if (!cities.length) {
+    return <Message message="Add your first city by clicking on the map" />;
+  }
   return (
-    <ul className={styles.CityList}>
+    <ul className={styles.cityList}>
       {cities.map((city, index) => {
-        return <li key={index}>{<CityItem city={city} key={city.id} />}</li>;
+        return <CityItem city={city} key={city.id} />;
       })}
     </ul>
   );
